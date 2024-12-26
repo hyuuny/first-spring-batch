5b9c6911f70d1aeaa25044288966a54b239fc176
hyuuny <shyune@knou.ac.kr>
2024-12-26 21:33:22 +0900

    feat: init project

---
+++ src/test/java/com/hyuuny/firstspringbatch/FirstSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyuuny.firstspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
