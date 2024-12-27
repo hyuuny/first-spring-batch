8470a401b0ef1978fc709dd61bf4d1cd05636a5f
hyuuny <shyune@knou.ac.kr>
2024-12-27 13:12:32 +0900

    feat: batch 작업을 위한 도메인 구현

---
+++ src/main/java/com/hyuuny/firstspringbatch/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package com.hyuuny.firstspringbatch.repository;
+
+import com.hyuuny.firstspringbatch.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
---
+++ src/main/java/com/hyuuny/firstspringbatch/repository/BeforeRepository.java
@@ -0,0 +1,7 @@
+package com.hyuuny.firstspringbatch.repository;
+
+import com.hyuuny.firstspringbatch.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+}
