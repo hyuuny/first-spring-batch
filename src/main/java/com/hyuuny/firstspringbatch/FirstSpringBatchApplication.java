3b329cca1340868e9e8b4a29dd629ef121b90831
hyuuny <shyune@knou.ac.kr>
2024-12-28 19:58:27 +0900

    feat: table to table Batch 작업을 위한 Schedule 구현

--- src/main/java/com/hyuuny/firstspringbatch/FirstSpringBatchApplication.java
+++ src/main/java/com/hyuuny/firstspringbatch/FirstSpringBatchApplication.java
@@ -2,12 +2,14 @@ package com.hyuuny.firstspringbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class FirstSpringBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstSpringBatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBatchApplication.class, args);
+    }
 
 }
